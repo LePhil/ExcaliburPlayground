1dbc64e25983e5bef1f7ef3de299e278fdc80edb
Philipp Christen <philipp.christen@gmail.com>
2017-06-17 14:50:03 +0200

    ..and the resources of course

---
+++ game/Resources.ts
@@ -0,0 +1,8 @@
+declare var globals: any;
+import * as ex from "excalibur";
+
+export class Resources {
+    static TextureMonsterDown  = new ex.Texture("game/assets/textures/minotaurv2.png");
+    static TextureMonsterRight = new ex.Texture("game/assets/textures/minotaurv2right.png");
+    static TextureMonsterUp    = new ex.Texture("game/assets/textures/minotaurv2back.png");
+}
