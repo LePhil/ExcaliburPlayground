d0ba91846525618bd26c56559869a9fb3ed99bc3
Philipp Christen <philipp.christen@gmail.com>
2017-09-06 17:57:35 +0200

    forgot AbstractPlayer and fixed ThinkBubble's position

---
+++ game/AbstractPlayer.ts
@@ -0,0 +1,93 @@
+declare var globals: any;
+import * as ex from "excalibur";
+
+export abstract class AbstractPlayer extends ex.Actor {
+  _speed: number;
+  private _lastPosX: number;
+  private _lastPosY: number;
+
+  constructor(x = globals.conf.PLAYER_STARTX,
+              y = globals.conf.PLAYER_STARTY,
+              w = globals.conf.PLAYER_WIDTH,
+              h = globals.conf.PLAYER_HEIGHT,
+              s = globals.conf.PLAYER_SPEED) {
+    super(x, y, w, h);
+
+    this._speed = s;
+
+    this._lastPosX = this.pos.x;
+    this._lastPosY = this.pos.y;
+  }
+
+  onInitialize(engine: ex.Engine): void {
+    let spriteSheet = new ex.SpriteSheet(globals.resources.TexturePlayers, 7, 8, 128, 256);
+
+    let myType = this._getPlayerColorIndex();
+    let scale = globals.conf.SPRITE_SCALE;
+    let speed = globals.conf.SPRITE_ANIM_SPEED;
+    let coords = globals.conf.PLAYER_TYPES[myType].coords;
+
+    let walkRightAnim = spriteSheet.getAnimationByIndices(engine, coords.walkR, speed);
+    walkRightAnim.loop = true;
+    walkRightAnim.scale.setTo(scale, scale);
+    this.addDrawing("walkRight", walkRightAnim);
+
+    let walkLeftAnim = spriteSheet.getAnimationByIndices(engine, coords.walkR, speed);
+    walkLeftAnim.loop = true;
+    walkLeftAnim.flipHorizontal = true;
+    walkLeftAnim.scale.setTo(scale, scale);
+    this.addDrawing("walkLeft", walkLeftAnim);
+
+    let idleSprite = spriteSheet.getSprite(coords.idle);
+    idleSprite.scale.setTo(scale, scale);
+    this.addDrawing("idle", idleSprite);
+
+    let walkUpAnim = spriteSheet.getAnimationByIndices(engine, coords.walkUp, speed);
+    walkUpAnim.loop = true;
+    walkUpAnim.scale.setTo(scale, scale);
+    this.addDrawing("walkUp", walkUpAnim);
+
+    let pickUpSprite = spriteSheet.getSprite(coords.pick);
+    pickUpSprite.scale.setTo(scale, scale);
+    this.addDrawing("pickUp", pickUpSprite);
+
+    // TODO: down anim not included in spritesheet :(
+    this.addDrawing("walkDown", idleSprite);
+
+    this.setDrawing("idle");
+  }
+
+  public update(engine: ex.Engine, delta: number): void {
+     super.update(engine, delta);
+
+     let xMovement = this._lastPosX - this.pos.x;
+     let yMovement = this._lastPosY - this.pos.y
+
+     if (Math.abs(xMovement) > Math.abs(yMovement) ) {
+       if (xMovement > 0) {
+         this.setDrawing("walkLeft");
+       } else if (xMovement < 0) {
+         this.setDrawing("walkRight");
+       }
+     } else {
+       if (yMovement > 0) {
+         this.setDrawing("walkUp");
+       } else if (yMovement < 0) {
+         this.setDrawing("walkDown");
+       }
+     }
+
+     if (yMovement === 0 && xMovement === 0) {
+       this.setDrawing("idle");
+     } else {
+       this._updateChildren();
+     }
+
+     this._lastPosX = this.pos.x;
+     this._lastPosY = this.pos.y;
+  }
+
+  abstract _getPlayerColorIndex ():number
+
+  abstract _updateChildren():void
+}
