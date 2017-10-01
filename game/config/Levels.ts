b3fad7d5011759527b3f14f8080d70fc3ee2468d
Philipp Christen <philipp.christen@gmail.com>
2017-10-01 20:33:23 +0200

    Intro 2 done

--- game/config/Levels.ts
+++ game/config/Levels.ts
@@ -111,7 +111,7 @@ export class Levels {
                 {Name: "phone_initial", X: 350, Y: 480},
             ],
             CHARACTERS: [
-                {Id: "hime", Name: "Player", Color: "green", Initial: "player_entry"}
+                {Id: "hime", Name: "Player", Color: "red", Initial: "player_entry"}
             ],
             PROPS: [
                 {Id: "phone",   Type: "mobile_phone", Initial: "phone_initial", Opacity: 0}
@@ -122,8 +122,7 @@ export class Levels {
                 {T: 10, S: "hime", A: ActionType.Talk, O: {text: "Finally done with school."} },
                 {T: 14, S: "hime", A: ActionType.Talk, O: {text: "Can't wait to go to University"} },
                 {T: 18, S: "hime", A: ActionType.Talk, O: {text: "and become a Psychologist!"} },
-                {T: 22, S: "hime", A: ActionType.Talk, O: {text: "But first, let's check SeenIt."} },
-                {T: 24, S: "hime", A: ActionType.Talk, O: {text: ""} },
+                {T: 22, S: "hime", A: ActionType.Talk, O: {text: "But first, let's check SeenIt.", duration: 2} },
                 {T: 24, S: "phone",A: ActionType.Show},
                 {T: 28, S: "hime", A: ActionType.Talk, O: {text: "Hihi! Silly cats..."} },
                 {T: 34, S: "hime", A: ActionType.Talk, O: {text: "Oh?"} },
@@ -131,8 +130,7 @@ export class Levels {
                 {T: 40, S: "hime", A: ActionType.Talk, O: {text: "'Psyche completely researched.'"} },
                 {T: 44, S: "hime", A: ActionType.Talk, O: {text: "'No more psych studies.'"} },
                 {T: 46, S: "phone",A: ActionType.Hide},
-                {T: 50, S: "hime", A: ActionType.Talk, O: {text: "Poop! What do I do now?"} },
-                {T: 54, S: "hime", A: ActionType.Talk, O: {text: ""} },
+                {T: 50, S: "hime", A: ActionType.Talk, O: {text: "Poop! What do I do now?", duration: 4} },
                 {T: 54, S: "hime", A: ActionType.Move, O: {to: "player_entry"} },
                 {T: 58, S: "hime", A: ActionType.Hide}                
             ],
@@ -148,15 +146,41 @@ export class Levels {
                 {Name: "player_entry",   X: 100, Y: 530},
                 {Name: "old_guy_entry",  X: 900, Y: 530},
                 {Name: "player_main",    X: 380, Y: 530},
+                {Name: "player_main_2",  X: 250, Y: 530},
                 {Name: "old_guy_main",   X: 460, Y: 530}
             ],
             CHARACTERS: [
-                {Id: "hime",   Name: "Player",  Color: "green",  Initial: "player_entry"},
-                {Id: "oldguy", Name: "Old Guy", Color: "yellow", Initial: "old_guy_entry"}
+                {Id: "hime",   Name: "Player",  Color: "red",  Initial: "player_entry"},
+                {Id: "oldguy", Name: "Old Guy", Color: "yellow", Initial: "old_guy_entry", Opacity: 0}
             ],
             PROPS: [],
             SCRIPT: [
-                {T: 3,  S: "hime", A: ActionType.Move, O: {to: "player_main"} }
+                {T: 2,  S: "hime", A: ActionType.Move, O: {to: "player_main"} },
+                {T: 6,  S: "hime", A: ActionType.Talk, O: {text: "What to do? What to do?", duration: 4} },
+                {T: 10, S: "hime", A: ActionType.Talk, O: {text: ""} },
+                {T: 10, S: "hime", A: ActionType.Move, O: {to: "player_main_2"} },
+                {T: 12, S: "hime", A: ActionType.Talk, O: {text: "My dream, destroyed!", duration: 2} },
+                {T: 14, S: "hime", A: ActionType.Move, O: {to: "player_main"} },
+                {T: 16, S: "hime", A: ActionType.Talk, O: {text: "*sob*", duration: 2} },
+                {T: 22, S: "oldguy",A: ActionType.Show},
+                {T: 22, S: "oldguy",A: ActionType.Move, O: {to: "old_guy_main"} },
+                {T: 24, S: "oldguy",A: ActionType.Talk, O: {text: "..."} },
+                {T: 26, S: "hime", A: ActionType.Talk, O: {text: "*sob*", duration: 2} },
+                {T: 28, S: "oldguy",A: ActionType.Talk, O: {text: "Oh noes! What happened?", duration: 3 } },
+                {T: 32, S: "hime", A: ActionType.Talk, O: {text: "Uncle!"} },
+                {T: 34, S: "hime", A: ActionType.Talk, O: {text: "I don't know what to do!"} },
+                {T: 37, S: "hime", A: ActionType.Talk, O: {text: "My future is doomed!", duration: 3} },
+                {T: 40, S: "oldguy",A: ActionType.Talk, O: {text: "Oh dear! What a pickle.", duration: 3} },
+                {T: 43, S: "hime", A: ActionType.Talk, O: {text: "*sob*", duration: 2} },
+                {T: 44, S: "oldguy",A: ActionType.Talk, O: {text: "But you're in luck!"} },
+                {T: 47, S: "oldguy",A: ActionType.Talk, O: {text: "I'm retiring tomorrow and"} },
+                {T: 50, S: "oldguy",A: ActionType.Talk, O: {text: "am looking for a successor.", duration: 3} },
+                {T: 53, S: "hime", A: ActionType.Talk, O: {text: "*sob*", duration: 2} },
+                {T: 55, S: "hime", A: ActionType.Talk, O: {text: "... really? Where?", duration: 2} },
+                {T: 57, S: "oldguy",A: ActionType.Talk, O: {text: "At my Rabbit Food Store!", duration: 3} },
+                {T: 60, S: "hime", A: ActionType.Talk, O: {text: "I don't know...", duration: 3} },
+                {T: 63, S: "oldguy",A: ActionType.Talk, O: {text: "Humbug! You can start tomorrow!", duration: 3} },
+                {T: 66, S: "oldguy",A: ActionType.Move, O: {to: "old_guy_entry"} }
             ],
             NEXT: "Map_00"
         }
