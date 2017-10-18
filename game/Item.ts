27307e24c85bb6a771fe1451127d215070ed8320
Philipp Christen <philipp.christen@gmail.com>
2017-10-18 22:02:03 +0200

    bye bye food

--- game/Item.ts
+++ game/Item.ts
@@ -1,13 +1,20 @@
 import * as ex from "excalibur";
+import {Player} from "./Player";
+
+export class ItemFactory {
+    static create(setup: any, player: Player): Item {
+        return new Item("TODO");
+    }
+}
 
 export class Item {
-  private _type: string;
+    private _type: string;
 
-  constructor(type: string) {
-    this._type = type;
-  }
+    constructor(type: string) {
+        this._type = type;
+    }
 
-  public getType(): string {
-    return this._type;
-  }
+    public getType(): string {
+        return this._type;
+    }
 }
